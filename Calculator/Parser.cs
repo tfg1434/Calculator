67c8ce4d5ad5f4f51b51cf060642181e5d3286ce
torontofangirl <toronto.tristan@gmail.com>
2021-04-29 17:52:45 -0500

    Parser class

---
+++ Calculator/Parser.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ExtensionMethods;
+using System.Text;
+using System.Threading.Tasks;
+using DecimalMath;
+
+namespace Calculator {
+    class Parser {
+        //parse a string into reverse polish notation
+        private readonly Lexer lexer;
+        private static readonly Dictionary<string, int> precedence = new() {
+            ["^"] = 4,
+            ["*"] = 3,
+            ["/"] = 3,
+            ["+"] = 2,
+            ["-"] = 2,
+        };
+        private const int right_associative = 0;
+        private const int left_associative = 1;
+        private static readonly Dictionary<string, int> associativity = new() {
+            ["^"] = right_associative,
+            ["*"] = left_associative,
+            ["/"] = left_associative,
+            ["+"] = left_associative,
+            ["-"] = left_associative,
+        };
+        private static readonly string[] operators = { "+", "-", "*", "/", "^" };
+        private static Dictionary<string, decimal> constants = new() {
+            ["e"] = DecimalEx.E,
+            ["pi"] = DecimalEx.Pi,
+        };
+
+        public List<string> Parse() {
+            Stack<string> tokens = new();
+            while (!lexer.empty) {
+                tokens.Push(lexer.Next());
+            }
+            tokens = tokens.Reverse();
+            return shunting_yard(tokens);
+        }
+
+        private List<string> shunting_yard(Stack<string> tokens) {
+            Stack<string> operator_stack = new();
+            List<string> rpn = new();
+
+            while (tokens.Count > 0) {
+                string token = tokens.Pop();
+                if (decimal.TryParse(token, out _)) {
+                    rpn.Add(token);
+
+                } else if (token.All(char.IsLetter)) {
+                    //operator
+                    operator_stack.Push(token);
+
+                } else if (operators.Contains(token)) {
+                    while (operator_stack.Count > 0 && operators.Contains(operator_stack.Peek()) &&
+                        (precedence[operator_stack.Peek()] > precedence[token] || (precedence[operator_stack.Peek()] == precedence[token] && associativity[token] == left_associative)) &&
+                        operator_stack.Peek() != "(") {
+                        rpn.Add(operator_stack.Pop());
+                    }
+
+                    operator_stack.Push(token);
+
+                } else if (token == "(") {
+                    operator_stack.Push(token);
+
+                } else if (token == ")") {
+                    while (operator_stack.Count > 0 && operator_stack.Peek() != "(") {
+                        rpn.Add(operator_stack.Pop());
+                    }
+                    //If the stack runs out without finding a left parenthesis, then there are mismatched parentheses.
+                    if (operator_stack.Count == 0) {
+                        throw new Exception("Mismatched parenthesis");
+                    }
+
+                    if (operator_stack.Count > 0 && operator_stack.Peek() == "(") {
+                        operator_stack.Pop();
+                    }
+                    if (operator_stack.Count > 0 && operator_stack.Peek().All(char.IsLetter)) {
+                        rpn.Add(operator_stack.Pop());
+                    }
+                }
+            }
+
+            if (tokens.Count == 0) {
+                while (operator_stack.Count > 0) {
+                    //If the operator token on the top of the stack is a parenthesis, then there are mismatched parentheses.
+                    if (operator_stack.Peek() == "(" || operator_stack.Peek() == ")") {
+                        throw new Exception("Mismatched Parentheses");
+                    }
+
+                    rpn.Add(operator_stack.Pop());
+                }
+            }
+
+            return rpn;
+        }
+
+        private string replace_constants(string equation) {
+            foreach (KeyValuePair<string, decimal> constant in constants) {
+                while (equation.Contains(constant.Key)) {
+                    int index = equation.IndexOf(constant.Key);
+                    if ((index == 0 || index == equation.Length - constant.Key.Length) || (!char.IsLetter(equation[index - 1]) && !char.IsLetter(equation[index + constant.Key.Length]))) {
+                        equation = equation.Remove(index, constant.Key.Length);
+                        equation = equation.Insert(index, constant.Value.ToString());
+                    }
+                }
+            }
+
+            return equation;
+        }
+
+        public Parser(string str) {
+            lexer = new Lexer(replace_constants(str));
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,14 +8,12 @@ namespace Calculator {
     class Program {
         static void Main(string[] args) {
             string equation = args[0];
-            List<string> rpn = ShuntingYard(equation);
-            decimal a = Solve(rpn);
-            int b = 5;
+            Parser parser = new(equation);
+            Console.WriteLine(Solve(parser.Parse()));
         }
 
-        static readonly string[] operators = { "+", "-", "*", "/", "^" };
-
         static decimal Solve(List<string> rpn) {
+            string[] operators = { "+", "-", "*", "/", "^" };
             Stack<string> stack = new();
 
             foreach (string token in rpn) {
@@ -40,12 +38,6 @@ namespace Calculator {
                 } else {
                     //function
                     switch (token) {
-                        case "e":
-                            stack.Push(Math.E.ToString());
-                            break;
-                        case "pi":
-                            stack.Push(Math.PI.ToString());
-                            break;
                         case "abs":
                             stack.Push(Math.Abs(decimal.Parse(stack.Pop())).ToString());
                             break;
@@ -77,14 +69,16 @@ namespace Calculator {
                             stack.Push(DecimalEx.Ceiling(decimal.Parse(stack.Pop())).ToString());
                             break;
                         case "clamp":
-                            //args backward because it's popped off backwards
-                            decimal max = decimal.Parse(stack.Pop());
-                            decimal min = decimal.Parse(stack.Pop());
-                            decimal val = decimal.Parse(stack.Pop());
+                            {
+                                //args backward because it's popped off backwards
+                                decimal max = decimal.Parse(stack.Pop());
+                                decimal min = decimal.Parse(stack.Pop());
+                                decimal val = decimal.Parse(stack.Pop());
 
-                            //Console.WriteLine($"val: {val}  min: {min}   max: {max}");
-                            stack.Push(Math.Clamp(val, min, max).ToString());
-                            break;
+                                //Console.WriteLine($"val: {val}  min: {min}   max: {max}");
+                                stack.Push(Math.Clamp(val, min, max).ToString());
+                                break;
+                            }
                         case "cos":
                             stack.Push(DecimalEx.Cos(decimal.Parse(stack.Pop())).ToString());
                             break;
@@ -92,12 +86,62 @@ namespace Calculator {
                             stack.Push((DecimalEx.Cos(decimal.Parse(stack.Pop())) * 180 / DecimalEx.Pi).ToString());
                             break;
                         case "mod":
-                            decimal divisor = decimal.Parse(stack.Pop());
-                            val = decimal.Parse(stack.Pop());
-                            stack.Push((val % divisor).ToString());
-                            break;
+                            {
+                                decimal divisor = decimal.Parse(stack.Pop());
+                                decimal val = decimal.Parse(stack.Pop());
+                                stack.Push((val % divisor).ToString());
+                                break;
+                            }
                         case "floor":
                             stack.Push(Math.Floor(decimal.Parse(stack.Pop())).ToString());
+                            break;
+                        case "ln":
+                            stack.Push(DecimalEx.Log(decimal.Parse(stack.Pop())).ToString());
+                            break;
+                        case "log":
+                            {
+                                decimal my_base = decimal.Parse(stack.Pop());
+                                decimal val = decimal.Parse(stack.Pop());
+                                stack.Push(DecimalEx.Log(val, my_base).ToString());
+                                break;
+                            }
+                        case "log10":
+                            stack.Push(DecimalEx.Log10(decimal.Parse(stack.Pop())).ToString());
+                            break;
+                        case "log2":
+                            stack.Push(DecimalEx.Log2(decimal.Parse(stack.Pop())).ToString());
+                            break;
+                        case "max":
+                            {
+                                decimal val2 = decimal.Parse(stack.Pop());
+                                decimal val1 = decimal.Parse(stack.Pop());
+                                stack.Push(Math.Max(val1, val2).ToString());
+                                break;
+                            }
+                        case "min":
+                            {
+                                decimal val2 = decimal.Parse(stack.Pop());
+                                decimal val1 = decimal.Parse(stack.Pop());
+                                stack.Push(Math.Min(val1, val2).ToString());
+                                break;
+                            }
+                        case "round":
+                            stack.Push(Math.Round(decimal.Parse(stack.Pop())).ToString());
+                            break;
+                        case "sign":
+                            stack.Push(Math.Sign(decimal.Parse(stack.Pop())).ToString());
+                            break;
+                        case "sin":
+                            stack.Push(DecimalEx.Sin(decimal.Parse(stack.Pop())).ToString());
+                            break;
+                        case "sqrt":
+                            stack.Push(DecimalEx.Sqrt(decimal.Parse(stack.Pop())).ToString());
+                            break;
+                        case "tan":
+                            stack.Push(DecimalEx.Tan(decimal.Parse(stack.Pop())).ToString());
+                            break;
+                        case "trunc":
+                            stack.Push(Math.Truncate(decimal.Parse(stack.Pop())).ToString());
                             break;
                     }
                 }
@@ -106,85 +150,6 @@ namespace Calculator {
             return decimal.Parse(stack.Pop());
         }
 
-        static List<string> ShuntingYard(string equation) {
-            Stack<string> equation_stack = new();
-            Lexer lexer = new(equation);
-            while (!lexer.empty) {
-                equation_stack.Push(lexer.Next());
-            }
-            equation_stack = equation_stack.Reverse();
-
-            Stack<string> operator_stack = new();
-            List<string> rpn = new();
-
-            Dictionary<string, int> precedence = new() {
-                ["^"] = 4,
-                ["*"] = 3,
-                ["/"] = 3,
-                ["+"] = 2,
-                ["-"] = 2,
-            };
-            const int right_associative = 0;
-            const int left_associative = 1;
-            Dictionary<string, int> associativity = new() {
-                ["^"] = right_associative,
-                ["*"] = left_associative,
-                ["/"] = left_associative,
-                ["+"] = left_associative,
-                ["-"] = left_associative,
-            };
-
-            while (equation_stack.Count > 0) {
-                string token = equation_stack.Pop();
-                if (decimal.TryParse(token, out _)) {
-                    rpn.Add(token);
-
-                } else if (token.All(char.IsLetter)){
-                    //operator
-                    operator_stack.Push(token);
-
-                } else if (operators.Contains(token)) {
-                    while (operator_stack.Count > 0 && operators.Contains(operator_stack.Peek()) &&
-                        (precedence[operator_stack.Peek()] > precedence[token] || (precedence[operator_stack.Peek()] == precedence[token] && associativity[token] == left_associative)) &&
-                        operator_stack.Peek() != "(") {
-                        rpn.Add(operator_stack.Pop());
-                    }
-
-                    operator_stack.Push(token);
-
-                } else if (token == "(") {
-                    operator_stack.Push(token);
-
-                } else if (token == ")") {
-                    while (operator_stack.Count > 0 && operator_stack.Peek() != "(") {
-                        rpn.Add(operator_stack.Pop());
-                    }
-                    //If the stack runs out without finding a left parenthesis, then there are mismatched parentheses.
-                    if (operator_stack.Count == 0) {
-                        throw new Exception("Mismatched parenthesis");
-                    }
-
-                    if (operator_stack.Count > 0 && operator_stack.Peek() == "(") {
-                        operator_stack.Pop();
-                    }
-                    if (operator_stack.Count > 0 && operator_stack.Peek().All(char.IsLetter)) {
-                        rpn.Add(operator_stack.Pop());
-                    }
-                }
-            }
-
-            if (equation_stack.Count == 0) {
-                while (operator_stack.Count > 0) {
-                    //If the operator token on the top of the stack is a parenthesis, then there are mismatched parentheses.
-                    if (operator_stack.Peek() == "(" || operator_stack.Peek() == ")") {
-                        throw new Exception("Mismatched Parentheses");
-                    }
-
-                    rpn.Add(operator_stack.Pop());
-                }
-            }
-
-            return rpn;
-        }
+        
     }
 }
